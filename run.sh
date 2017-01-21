1f6ccb150edb9a9bc028578ffd6b64695d15dd27
root <tzh.nut@gmail.com>
2017-01-21 07:10:54 -0500

    STORMHEART UPDATED PRIVATE REPOSITORY: c9.io/nutiler

---
+++ run.sh
@@ -0,0 +1,1 @@
+java -Xms5G -Xmx5G -XX:+UseG1GC -XX:+UnlockExperimentalVMOptions -XX:MaxGCPauseMillis=100 -XX:+DisableExplicitGC -XX:TargetSurvivorRatio=90 -XX:G1NewSizePercent=50 -XX:G1MaxNewSizePercent=80 -XX:InitiatingHeapOccupancyPercent=10 -XX:G1MixedGCLiveThresholdPercent=50 -XX:+AggressiveOpts -XX:+AlwaysPreTouch -XX:+UseLargePagesInMetaspace -jar paperclip.jar
